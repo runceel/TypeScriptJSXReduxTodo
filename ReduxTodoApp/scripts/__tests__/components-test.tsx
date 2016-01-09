9eae561596e335288827562815ee8973bc38193c
Kazuki Ohta <k_ota28@hotmail.com>
2016-01-10 01:16:57 +0900

    add component tests

---
+++ ReduxTodoApp/scripts/__tests__/components-test.tsx
@@ -0,0 +1,72 @@
+﻿/// <reference path="../../typings/jest/jest.d.ts" />
+
+jest.dontMock('../components');
+jest.dontMock('../models');
+import * as Components from '../components';
+import * as Models from '../models';
+import * as React from 'react';
+import TestUtils = require('react-addons-test-utils');
+
+describe('components tests', () => {
+    it('TodoComposer not completed', () => {
+        var todo = new Models.Todo('test');
+        todo.id = 1;
+
+        var mockHandler = jest.genMockFunction();
+        var todoComposer = TestUtils.renderIntoDocument(
+            <Components.TodoComposer todo={todo} onToggle={mockHandler} />);
+        var span = TestUtils.findRenderedDOMComponentWithTag(todoComposer, 'span');
+        expect(span.textContent).toBe('test');
+        expect((span as any).style.textDecoration).toBe('none');
+        TestUtils.Simulate.click(span);
+        expect(mockHandler).toBeCalled();
+    });
+
+    it('TodoComposer completed', () => {
+        var todo = new Models.Todo('test', true);
+        todo.id = 1;
+
+        var mockHandler = jest.genMockFunction();
+        var todoComposer = TestUtils.renderIntoDocument(
+            <Components.TodoComposer todo={todo} onToggle={mockHandler} />);
+        var span = TestUtils.findRenderedDOMComponentWithTag(todoComposer, 'span');
+        expect(span.textContent).toBe('test');
+        expect((span as any).style.textDecoration).toBe('line-through');
+        TestUtils.Simulate.click(span);
+        expect(mockHandler).toBeCalled();
+    });
+
+    it('TodoList', () => {
+        var todos: Models.Todo[] = [
+            new Models.Todo('test1'),
+            new Models.Todo('test2'),
+            new Models.Todo('test3')
+        ];
+        todos[0].id = 0;
+        todos[1].id = 1;
+        todos[2].id = 2;
+        var mockHandler = jest.genMockFunction();
+        var todoListComposer = TestUtils.renderIntoDocument(
+            <Components.TodoListComposer todos={todos} onToggle={mockHandler} />);
+        var spans = TestUtils.findAllInRenderedTree(todoListComposer, (x: any) => {
+            return x.tagName && x.tagName.toLowerCase() === 'span';
+        });
+        expect(spans.length).toBe(3);
+        TestUtils.Simulate.click(spans[1] as Element);
+        expect(mockHandler).toBeCalledWith(todos[1].id);
+    });
+
+    it('TodoFormComposer', () => {
+        var mockHandler = jest.genMockFunction();
+        var formComposer = TestUtils.renderIntoDocument(
+            <Components.TodoFormComposer onAddTodo={mockHandler}/>);
+
+        var text = TestUtils.findRenderedDOMComponentWithTag(formComposer, 'form').childNodes[0] as HTMLInputElement;
+        var submit = TestUtils.findRenderedDOMComponentWithTag(formComposer, 'form').childNodes[1] as HTMLInputElement;
+        text.value = 'test';
+        TestUtils.Simulate.submit(submit);
+
+        expect(mockHandler).toBeCalledWith('test');
+        expect(text.value).toBe('');
+    });
+});
---
+++ ReduxTodoApp/scripts/__tests__/reducers-test.ts
@@ -0,0 +1,47 @@
+﻿/// <reference path="../../typings/jest/jest.d.ts" />
+
+jest.dontMock('../reducers');
+jest.dontMock('../actions');
+jest.dontMock('../models');
+import * as Reducers from '../reducers';
+import * as Actions from '../actions';
+import * as Models from '../models';
+import * as Redux from 'redux';
+
+describe('reducers test', () => {
+    it('initial state', () => {
+        var state = Redux.createStore(Reducers.todoApp);
+        var result = <Models.TodoList>state.getState().todos;
+        expect(Models.TodoUtils.toList(result.todos).length).toBe(0);
+    });
+
+    it('crud', () => {
+        // init
+        var state = Redux.createStore(Reducers.todoApp);
+        // add
+        state.dispatch(Actions.addTodo('test'));
+        var result = <Models.TodoList>state.getState().todos;
+        var list = Models.TodoUtils.toList(result.todos);
+        expect(list.length).toBe(1);
+        expect(list[0].text).toBe('test');
+        expect(list[0].completed).toBe(false);
+
+        var id = list[0].id;
+        // toggle
+        state.dispatch(Actions.toggleTodo(id));
+        result = <Models.TodoList>state.getState().todos;
+        list = Models.TodoUtils.toList(result.todos);
+        expect(list[0].completed).toBe(true);
+        // toggle
+        state.dispatch(Actions.toggleTodo(id));
+        result = <Models.TodoList>state.getState().todos;
+        list = Models.TodoUtils.toList(result.todos);
+        expect(list[0].completed).toBe(false);
+
+        // delete
+        state.dispatch(Actions.deleteTodo(id));
+        result = <Models.TodoList>state.getState().todos;
+        list = Models.TodoUtils.toList(result.todos);
+        expect(list.length).toBe(0);
+    });
+});
